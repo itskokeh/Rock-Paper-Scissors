7141007dc2c58608ba029dd344402a589f65cddd
okey-js <kokehinc@gmail.com>
2022-10-27 15:28:01 +0100

    add loop

--- start.js
+++ start.js
@@ -1,31 +1,74 @@
-computerOptions = ["rock", "paper", "scissors"];
-getComputerChoice =
-  computerOptions[Math.floor(Math.random() * computerOptions.length)];
-computerSelection = getComputerChoice;
-console.log(getComputerChoice);
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = "";
 
-select = prompt("Rock, Paper or Scissors", "");
+
+let computerOptions = ["rock", "paper", "scissors"];
+function getComputerChoice(){
+  return computerOptions[Math.floor(Math.random() * computerOptions.length)];
+}
+computerSelection = getComputerChoice();
+computerChoice = computerSelection;
+console.log(computerSelection);
+
+function selectG(){
+  return prompt("Rock, Paper or Scissors", "");
+}
+select = selectG();
 playerSelection = select;
 console.log(playerSelection);
 
-function playRound(playerSelection, computerSelection) {
-  if (select === getComputerChoice) {
+function playRound(selectG, computerChoice){
+  if (
+    (select == "paper" && computerChoice == "paper") || 
+    (select == "rock" && computerChoice == "rock") ||
+    (select == "scissors" && computerChoice == "scissors")) 
+    {
+    roundWinner = "tie";
     console.log("Tie");
-  } else if (
-    (select == "paper" && getComputerChoice == "rock") ||
-    (select == "rock" && getComputerChoice == "scissors") ||
-    (select == "scissors" && getComputerChoice == "paper")
+  } if (
+    (selectG == "paper" && computerChoice == "rock") ||
+    (selectG == "rock" && computerChoice == "scissors") ||
+    (selectG == "scissors" && computerChoice == "paper")
   ) {
+    playerScore++;
+    roundWinner = "player";
     console.log("You Win");
-  } else if (
-    (getComputerChoice == "paper" && select == "rock") ||
-    (getComputerChoice == "rock" && select == "scissors") ||
-    (getComputerChoice == "scissors" && select == "paper")
+  } if (
+    (computerChoice == "paper" && select == "rock") ||
+    (computerChoice == "rock" && select == "scissors") ||
+    (computerChoice == "scissors" && select == "paper")
   ) {
+    computerScore++;
+    roundWinner = "computer";
     console.log("Comp Wins");
-  } else {
-    console.log("void");
   }
-  return;
 }
-console.log(playRound(playerSelection, computerSelection));
+function game(){
+  playRound(select,computerChoice)
+  selectG()
+  getComputerChoice()
+}
+
+function declareWinner(){
+  return playerScore === 5 || computerScore === 5;
+}
+
+// function game(){
+//   for(let i = 0; i < 5; i++){
+//     selectG(playRound(select, computerChoice))
+//   }
+//   declareWinner()
+// }
+for (let i = 0; i < 5; i++){
+  if (playerScore < 5 || computerScore < 5){
+    game();
+  } else {
+    declareWinner();
+    console.log(i);
+  }
+}
+
+console.log(playerScore);
+console.log(computerScore);
+console.log(roundWinner);
